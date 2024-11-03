f218278a33e54d576d72625aea9b4668199fc823
dlmarques <daniel.marquesedigital@gmail.com>
2024-11-03 17:16:50 +0000

    Get all payments by lease id

--- routes/payments.js
+++ routes/payments.js
@@ -4,4 +4,10 @@ const controller = require("../controllers/payments.controller");
 
 router.post("/PayRent", [verifyToken], controller.pay);
 
+router.post(
+  "/GetAllPaymentsByLeaseId",
+  [verifyToken],
+  controller.getAllPaymentsByLease
+);
+
 module.exports = router;
